86ca0d2e40b96659bc4e33d7ef958fe48c6e2495
diegodipardo <diegodipardo@gmail.com>
2021-05-10 20:28:45 -0300

    index orden

--- assets/js/script.js
+++ assets/js/script.js
@@ -90,3 +90,18 @@ $(document)
     }
     e.preventDefault(); // prevent the default action (scroll / move caret)
   });
+
+  window.addEventListener('scroll', function()  {
+    let elements = document.getElementsByClassName('scroll-content');
+    let screenSize = window.innerHeight;
+    
+      for(var i = 0; i < elements.length; i++) {
+        let element = elements[i];
+  
+        if(element.getBoundingClientRect().top < screenSize) {
+          element.classList.add('visible');
+        } else {
+          element.classList.remove('visible');
+        }
+      }
+  });
